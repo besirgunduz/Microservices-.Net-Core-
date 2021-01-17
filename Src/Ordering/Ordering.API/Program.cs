5b4b1dceaa2612dc0c024136a0dec201ecd6b14e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-01-17 23:46:47 +0300

    bug fixed

---
+++ Src/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
